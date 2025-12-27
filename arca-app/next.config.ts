793ff3789712dc8c17233f0d8316e78592e3d75d
Adi Tauqir <aditauqir@gmail.com>
2025-12-27 11:12:19 -0500

    Added docker dev and prod + docs

--- arca-app/next.config.ts
+++ arca-app/next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from "next";
 
+// next.config.js
+module.exports = {
+  // ... rest of the configuration.
+  output: "standalone",
+};
 const nextConfig: NextConfig = {
   /* config options here */
 };
